0ab9e473fc8d977a8a2615a60aad0ac0fedd38ae
cmichailidis <cmichailidis98@gmail.com>
2017-05-09 16:10:12 +0300

    added immunity competition class to our project

--- ImmunityCompetition.h
+++ ImmunityCompetition.h
@@ -2,5 +2,25 @@
 #define IMMUNITYCOMPETITION_H_INCLUDED
 
 // TODO Fill this Up!!!
+#include <iostream>
+#include "Competition.h"
+#include "Team.h"
+#include "Award.h"
+#include "ImmunityAward.h"
+
+using namespace std;
+
+class ImmunityCompetition:public Competition {
+protected:
+    ImmunityAward immunityAward;
+public:
+    void setImmunityAward(ImmunityAward immunityAward);
+    ImmunityAward getImmunityAward(void);
+    ImmunityCompetition();
+    ImmunityCompetition(int id, string name, ImmunityAward immunityAward);
+    ~ImmunityCompetition();
+    void status(void);
+    void compete(Team &team);
+};
 
 #endif // IMMUNITYCOMPETITION_H_INCLUDED
