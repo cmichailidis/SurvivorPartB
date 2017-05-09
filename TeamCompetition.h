348d11fe6e3932a01adbc7463d254f2affbfee1b
student <alextsigilis@gmail.com>
2017-05-09 15:04:44 +0300

    TeamAward

--- TeamCompetition.h
+++ TeamCompetition.h
@@ -3,4 +3,27 @@
 
 // TODO Fill this Up!!!
 
+#include "Competition.h"
+#include "FoodAward"
+#include "Round.h"
+
+class TeamComp : public Comp {
+protected:
+	// This is the Food Award variable
+	FoodAward foodAward;
+	// An Array or rounds, as it apears from the vairbles name 'rounds'
+	Round rounds[19];
+
+public:
+	// Constructor
+	TeamComp():Comp(){ foodAward = FoodAward(); for(int i=0;i<19;++i)rounds[i] = Round();};
+	TeamComp(int id, string name, FoodAward f):TeamComp(id,name);
+	
+	// Destructor
+	~TeamComp():~Comp() {cout << "TeamComp with the beautifull name " << this.name << " just destroyed." << endl; };
+
+	// Setters
+	void setFoodAward(FoodAward f){ this.foodAward = f;};
+	void setRound(int i, Round 
+
 #endif // TEAMCOMPETITION_H_INCLUDED
